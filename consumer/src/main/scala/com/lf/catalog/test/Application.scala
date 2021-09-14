c73801445162d45ad91726a6ab8e143fd2db5cc4
rim99 <zhangxin086@gmail.com>
2021-09-14 22:36:11 +0800

    1. allow app to ramp up with manual multiple testing rounds 2. Pretty print

--- consumer/src/main/scala/com/lf/catalog/test/Application.scala
+++ consumer/src/main/scala/com/lf/catalog/test/Application.scala
@@ -3,10 +3,31 @@ package com.lf.catalog.test
 import akka.actor.typed.ActorSystem
 import akka.event.slf4j.Logger
 
+import java.util.Scanner
+import scala.annotation.tailrec
+
 object Application extends App {
-  val testing: ActorSystem[Config] = ActorSystem(Tester(User), "tester")
+  val log = Logger("App")
   val testConfig = Config.collect()
-  testing ! testConfig
+  val sc = new Scanner(System.in)
+  val guide = "Please hit enter to trigger test. Type in `exit` to close application"
+  log.info(guide)
+
+  @tailrec
+  def run(): Unit = {
+    val content = sc.nextLine()
+    if (content.equalsIgnoreCase("exit")) {
+      log.info("Closed")
+      System.exit(0)
+    } else {
+      log.info("Testing, please don't touch keyboard")
+      val testing: ActorSystem[Config] = ActorSystem(Tester(User), "tester")
+      testing ! testConfig
+    }
+    run()
+  }
+
+  run()
 }
 
 object User extends OperatorFactory {
--- consumer/src/main/scala/com/lf/catalog/test/ResultPrinter.scala
+++ consumer/src/main/scala/com/lf/catalog/test/ResultPrinter.scala
@@ -10,7 +10,24 @@ case class Result(p50LatencyInMs: Double,
                   p999LatencyInMs: Double,
                   maxLatencyInMs: Double,
                   avgThroughPutInSec: Double,
-                  maxThroughPutInSec: Double)
+                  maxThroughPutInSec: Double) {
+  override def toString: String =
+    s"""
+      | Test Result:
+      |   * Latency(ms):
+      |     - p50:     ${p50LatencyInMs}
+      |     - p90:     ${p90LatencyInMs}
+      |     - p99:     ${p99LatencyInMs}
+      |     - p999:    ${p999LatencyInMs}
+      |     - max:     ${maxLatencyInMs}
+      |  * ThroughPut(op/s):
+      |     - average: ${avgThroughPutInSec}
+      |     - max:     ${maxThroughPutInSec}
+      |
+      |
+      |Please hit enter to trigger test. Type in `exit` to close application
+      |""".stripMargin
+}
 
 object ResultPrinter {
   private val log = Logger("ResultPrinter")
